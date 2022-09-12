d66bacd742cf5d4c48be3f8aa2a605ed2240608b
radovanbotik <radovanbotik@gmail.com>
2022-09-12 20:51:09 +0200

    google icons + html validation

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const contactControlIcon = document.querySelector(".contact-control__icon");
 const contactControl = document.querySelector(".contact-control");
+const iconImage = document.querySelector(".icon-image");
 
 console.log(contactControlIcon, contactControl);
 
-const addActive = () => {
+const toggleActive = () => {
   contactControl.classList.toggle("active");
+  if (iconImage.textContent.includes("close")) {
+    iconImage.textContent = "mail";
+  } else {
+    iconImage.textContent = "close";
+  }
 };
 
-contactControlIcon.addEventListener("click", addActive);
+contactControlIcon.addEventListener("click", toggleActive);
